a0ffb8f2b738e76c5ab3f3b2b215aa838bbe0d9c
Mariam Ibrahim <ibrahimoyiza198@gmail.com>
2023-10-06 15:44:07 +0100

    add functionality add more features to calculator such as keyboard support, sign change, ability to calculate percentages update html and css

--- script.js
+++ script.js
@@ -3,24 +3,29 @@
  const buttons = document.querySelectorAll('.button')
  const numButtons = document.querySelectorAll('.digit_button');
  const operatorButtons = document.querySelectorAll('.operator_button');
+ const percentageButton = document.getElementById('percentage')
  const equalToButton = document.getElementById('equal-to');
- const clearButton = document.querySelector('.clear')
+ const clearButton = document.getElementById('clear');
+ const signButton = document.getElementById('negate')
 // event listeners
-buttons.forEach(button => button.addEventListener('click', getInput)) 
-numButtons.forEach( button => button.addEventListener('click', handleInputDisplay))
-operatorButtons.forEach( button => button.addEventListener('click', handleInputDisplay))
-equalToButton.addEventListener('click',handleOutputDisplay)
-clearButton.addEventListener('dblclick',clearAllDisplay);
-// variables
-let firstOperand, operator, secondOperand,result;
-let inputValues = '';
-// functions 
+buttons.forEach(button => button.addEventListener('click', getInput)) ;
+numButtons.forEach( button => button.addEventListener('click', handleInputDisplay));
+percentageButton.addEventListener('click',getPercentage);
+operatorButtons.forEach( button => button.addEventListener('click', handleInputDisplay));
+clearButton.addEventListener('click',clearAllDisplay);
+equalToButton.addEventListener('click',handleOutputDisplay);
+signButton.addEventListener('click', toggleSign)
+document.addEventListener('keypress', handleKeyboardPress);
 
+// variables
+let firstOperand, operator, secondOperand, result;
+let inputValues = ''
+let toggleEvent = false;
+// functions to handle operations
 function handleInputDisplay(e){ 
     const clickedButton = e.target
-    inputDisplay.textContent += (clickedButton.textContent)
+    inputDisplay.textContent += (clickedButton.textContent);
 }
-
 function addition(a,b){
     return a + b;
  }
@@ -31,34 +36,64 @@ function multiplication(a,b){
     return a * b;
 }
 function division(a,b){
-    if (b===0){
-       return "Math Error!"
-    }
     return a / b;
 }
 function percentage(a){
     return a / 100;
 }
+// handle decimal places
+function handleDecimal(a,b){
+        return parseFloat(`${a}.${b}`)
+}
+//functions to handle input and math evaluation
+function getPercentage(){
+    result = percentage(firstOperand);
+    firstOperand = result;
+    outputDisplay.textContent= firstOperand;
+    inputValues='';
+    return firstOperand;
+ }
+// function evaluateDecimal(){
+//    return result = handleDecimal(firstOperand, secondOperand);
+    
+// }
 function getInput(e){
     const clickedButton = e.target;
     inputValues += (clickedButton.textContent + '');
-    console.log(inputValues);
-    console.log(evaluateInput());
+    evaluateInput()
 }
 function evaluateInput(){
+    // ensure input values are taken as strings
     let expression = inputValues.toString();
-    let tokens = expression.split(/([+-/%×])/);
+    // split the input into array for evaluation
+    let tokens = expression.split(/([+-/÷%×.])/);
     firstOperand = parseFloat(tokens[0]);
+    // to evaluate negative values
+    if(toggleEvent){
+        firstOperand = -firstOperand
+    };
+    // input evaluation;
     for (let i=1; i < tokens.length; i += 2){
         operator= `${tokens[i]}`;
         secondOperand = parseFloat(tokens[i+1]);
-        operate(firstOperand,operator,secondOperand);
-   };
-   return (console.log(`${firstOperand}+ ${operator} + ${secondOperand}`))
-}
 
+        operate(firstOperand,operator,secondOperand);
+        roundupResult()
+        outputDisplay.textContent = result;
+        firstOperand = result;
+   }
+   if (isNaN(result)){
+        outputDisplay.textContent = '';
+   }
+   console.log(tokens);
+   return firstOperand;
+}
 function operate(firstOperand, operator, secondOperand){
     switch(operator){
+        case '.':
+            // to handle decimal input
+            
+        break;
         case '+':
             return result = addition(firstOperand,secondOperand);
         break;
@@ -68,36 +103,81 @@ function operate(firstOperand, operator, secondOperand){
         case '×':
             return result = multiplication(firstOperand ,secondOperand);   
         break;
-        case '/':
-            return result = division(firstOperand,secondOperand);   
+        case '÷':
+            if( secondOperand === 0){
+                // error validation for division
+                return  result = "Math Error!"
+             }else{
+                return result = division(firstOperand,secondOperand); 
+            } 
         break;
-        case '%':
-            return result = percentage(firstOperand);
+        case '.':
+            return result = handleDecimal(firstOperand, secondOperand);
         break;
     }
-    if(firstOperand && !operator && !secondOperand){
-        return result = firstOperand;
-    }
-    if(!operator || isNaN(result) || isNaN(secondOperand)){
+    // error validation of incorrectly entered expressions
+    if(!operator || !(firstOperand) || !(secondOperand)){
         return result = 'Invalid!'
     }
 }
+function roundupResult(){
+    let precison = result.toString().length;
+    // round answer to five figures
+    if (precison >= 10){
+        return result = result.toFixed(5);
+    }else{
+        return result;
+    }
+}
+// function to handle output 
 function handleOutputDisplay(){
-    evaluateInput()
     clearinputDisplay()
     outputDisplay.textContent = result;
 }
+// function to clear displays and array
 function clearinputDisplay(){
     inputDisplay.textContent = '';
     inputValues= '';
     console.log('clearedddd')
 }
+
 function clearAllDisplay(){
     outputDisplay.textContent = '';
     clearinputDisplay()
 }
-// function deleteNumber(){
-    // inputDisplay.textContent -= inputDisplay.textContent;
-// }
+// function to handle keyboard events
+function handleKeyboardPress(e) {
+    // Get buttons by data-key attribute for keys
+    const keys = document.querySelectorAll(`button[data-key="${e.key}"]`);
+    // Handle the equal to using space bar (if needed)
+    if (e.key === " ") {
+        handleOutputDisplay();
+    }
+    //handle clear using delete key
+    if (e.key === "Delete") {
+        clearAllDisplay();
+    }
+   
+    // Check if any operator key matches the pressed key
+        keys.forEach((key) => {
+            if (e.key === "%"){
+             return getPercentage()
+            }
+            inputDisplay.textContent += key.textContent;
+            inputValues += key.textContent;
+            evaluateInput();
+            console.log(inputValues)
+        });
+        
+    }
+// function to handle toggling signs on inputvalues
+function toggleSign() {
+    // Check if the firstOperand has a value
+    if (typeof firstOperand === "number") {
+        firstOperand = -firstOperand; 
+    }
+    toggleEvent = true;
+    inputDisplay.textContent = firstOperand;
+}
 
-
+clearAllDisplay()
